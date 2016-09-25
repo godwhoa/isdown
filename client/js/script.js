30f2127b3718448bb417b4cc5a532764e05bd64c
gjdaniel1999 <gjdaniel1999@gmail.com>
2016-09-25 16:35:10 +0530

    Re-do client with vanilla css

--- client/js/script.js
+++ client/js/script.js
@@ -1,44 +1,17 @@
-var upel = $('.hero.is-primary')
-var downel = $('.hero.is-danger')
-var time = 1250
+function check_site(url) {
+    $.getJSON('/list',
+        function(list) {
+            for (var i = 0; i < list.length; i++) {
+                var minion = list[i]
+                $.post(minion + '/isdown', {
+                    url: 'payload'
+                }, function(res) {
+                    if (res == "false"){
+                    }else{
 
-function showstatus(status) {
-	if (status == "up") {
-		upel.css("display","block")
-		setTimeout(function(){
-			upel.css("display","none")
-		},time)
-	} else if (status == "down"){
-		downel.css("display","block")
-		setTimeout(function(){
-			downel.css("display","none")
-		},time)
-	} else{
-		upel.css("display","none")
-		downel.css("display","none")
-	}
+                    }
+                })
+            }
+        }
+    });
 }
-
-function sitestatus(url){
-	var list
-	var statuses = ""
-	$.getJSON('/list', function(data){
-	  for (var i = 0;i<data.length;i++) {
-	  	var url = data[i]
-		$.post('/isdown',{"url":url},function(res){
-			if (res == "false"){
-				status = "up"
-			}else {
-				status = "down"
-			}
-		})
-	}
-	})
-	return status
-}
-
-$("#isdown-btn").on('click',function(e){
-	var site = $("#url").val()
-	var status = sitestatus(site)
-	showstatus(status)
-});
